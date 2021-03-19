8b42e1e5ac0cd305ae42c89b6a532cbbd7856e4b
Koki Kudo <massanmesu@icloud.com>
2021-03-19 16:33:20 +0900

    セルのレイアウトがひとまず完成。 セル内のボタンを押すとどのボタンが押されたか出力される関数を実装

--- TwitterHomePractice/TableViewCell.swift
+++ TwitterHomePractice/TableViewCell.swift
@@ -9,7 +9,28 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var content: UILabel!
     
+    
+    @IBAction func actionButton(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        
+        switch button.tag {
+        case 1:
+            print("リプライボタンが押されました。")
+        case 2:
+            print("リツイートボタンが押されました。")
+        case 3:
+            print("いいねボタンが押されました。")
+        case 4:
+            print("共有ボタンが押されました。")
+        default:
+            print("Error: どのボタンが押されたかわかりません。")
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- TwitterHomePractice/ViewController.swift
+++ TwitterHomePractice/ViewController.swift
@@ -4,19 +4,34 @@
 //
 //  Created by kudo koki on 2021/03/17.
 //
-
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let item = ["cell1", "cell2", "cell3", "cell4", "cell5"]
+    var item = [
+        ["name": "田中",
+         "content": "会食やら飲み会やらGotoトラベルやらでウェーイってやってる人だけがコロナに罹るならまだ自業自得と言えるけどそうではないからなー。",
+         "date": Date().timeIntervalSince1970 - 313123123],
+        ["name": "清水",
+         "content": "Gotoトラベルの段階的再開に向けた動きあり。「4月中の再開を目指す。」「5月中には全国拡大したい。」とのことです。",
+         "date": Date().timeIntervalSince1970 - 2323232],
+        ["name": "吉田",
+         "content": "コロナ対策に打つ手がないといっていた政権が「Gotoトラベル」再開を目指す？",
+         "date": Date().timeIntervalSince1970 - 3333],
+        ["name": "京都新聞",
+         "content": "Gotoトラベル、4月部分再開目指す。政府、大型連休には全国拡大案も(京都新聞)",
+         "date": Date().timeIntervalSince1970 - 10]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        // セルを降順にソート
+        item = item.reversed()
         // UITableViewのインスタンス生成
         let tableView = UITableView(frame: self.view.bounds, style: .plain)
+        // .xibのセルを反映させる
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
         // デリゲートを設定しプロトコルの設定を自分自身に委譲
         tableView.delegate = self
         tableView.dataSource = self
@@ -31,12 +46,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // セルを構成する際の情報
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // セルのIdentifierを指定しセルオブジェクトを作成
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        // セルに配置されている情報を設定
-        cell.textLabel?.text = item[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! TableViewCell
+        
+        // 時間を設定
+        let nowTime = Date().timeIntervalSince1970
+        let past = item[indexPath.row]["date"] as! TimeInterval
+        
+        // セル内の各オブジェクトに値を設定
+        cell.time.text = timeCheck(now: nowTime, past: past)
+        cell.content.text = (item[indexPath.row]["content"] as! String)
+        cell.name.text = (item[indexPath.row]["name"] as! String)
         return cell
     }
     
-    
+    // 時間の表示形式を決定する関数
+    func timeCheck(now: TimeInterval, past: TimeInterval) -> String {
+        // 今の時間とツイートした時間の差
+        let timeDiff = Int(now - past)
+        
+        // 時間差は秒単位の値なので、各単位に変換可能か確認
+        let year = timeDiff / 31104000
+        let month = timeDiff / 2592000
+        let day = timeDiff / 86400
+        let hour = timeDiff / 3600
+        let minute = timeDiff / 60
+        let second = timeDiff
+        
+        if year != 0 {
+            return "\(year)年前"
+        } else if month != 0 {
+            return "\(month)ヶ月前"
+        } else if day != 0 {
+            return "\(day)日前"
+        } else if hour != 0 {
+            return "\(hour)時間前"
+        } else if minute != 0 {
+            return "\(minute)分前"
+        } else {
+            return "\(second)秒前"
+        }
+    }
 }
 
